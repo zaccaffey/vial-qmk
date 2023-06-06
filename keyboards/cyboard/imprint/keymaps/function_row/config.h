daf7622a631bbbd95562c6dbe6e46d2810e2a647
Erik Trinkle <ectrink@gmail.com>
2023-06-06 13:25:18 -0400

    create imprint config with special request form factor

---
+++ keyboards/cyboard/imprint/keymaps/function_row/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xAB, 0x85, 0x24, 0x3E, 0xAB, 0x9B, 0x86, 0xCB}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 10, 10 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 6 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
