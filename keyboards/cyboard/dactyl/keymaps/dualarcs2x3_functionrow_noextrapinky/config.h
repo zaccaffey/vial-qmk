487f395bf9b4af4705d689e2af448e56ec920c6e
Erik Trinkle <ectrink@gmail.com>
2023-08-28 09:35:13 -0400

    Revert "remove non-standard dactyl configs which don't have info.json layouts yet"
    This reverts commit 4e367116371fc134ff7d48fa0675e832689fc800.

---
+++ keyboards/cyboard/dactyl/keymaps/dualarcs2x3_functionrow_noextrapinky/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC5, 0xAA, 0x8A, 0x4E, 0xF5, 0xEB, 0x3B, 0x19}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 10, 10 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 6 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
