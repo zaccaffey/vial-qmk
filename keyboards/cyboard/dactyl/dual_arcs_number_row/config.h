29cdc7493d3bc651347d81a937a6f7a001b8ff14
Erik Trinkle <ectrink@gmail.com>
2023-10-03 12:20:15 -0400

    move dual arcs number row into a new format that will allow simpler changes in the RGB matrix between variants.

---
+++ keyboards/cyboard/dactyl/dual_arcs_number_row/config.h
@@ -0,0 +1,10 @@
+/* Copyright 2023 Cyboard LLC (@Cyboard-DigitalTailor)
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+/* RGB settings */
+#ifdef RGB_MATRIX_ENABLE
+#    define RGB_MATRIX_LED_COUNT 66
+#endif
