92943178ff87e7df50c1f36920916cd8fcda84a5
Erik Trinkle <ectrink@gmail.com>
2024-03-12 18:33:27 -0400

    remove drag scroll in default layout
    since it interferes with vial mapping

--- keyboards/cyboard/imprint/imprint_function_row/keymaps/vial/keymap.c
+++ keyboards/cyboard/imprint/imprint_function_row/keymaps/vial/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                           KC_LGUI,   KC_LALT,   KC_LGUI, KC_ENT, KC_DEL,           KC_BSPC,  KC_SPC,  MO(1), KC_LBRC,  KC_RBRC,
-                                            DRGSCRL, KC_END,    KC_ESC,           KC_GRAVE,  KC_CAPS,  KC_APP
+                                            KC_HOME, KC_END,    KC_ESC,           KC_GRAVE,  KC_CAPS,  KC_APP
     ),
 
     [1] = LAYOUT_fun(
