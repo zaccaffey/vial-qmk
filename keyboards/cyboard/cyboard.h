900c278ffd896930aa546a375735175185453bb3
Erik Trinkle <ectrink@gmail.com>
2024-04-11 13:13:34 -0400

    adjust so that SPLIT_POINTING_ENABLE is the trigger for custom keycodes.
    now the vial keycodes work, but only for whichever half is plugged in.

--- keyboards/cyboard/cyboard.h
+++ keyboards/cyboard/cyboard.h
@@ -20,7 +20,7 @@
 
 #include "quantum.h"
 
-#ifdef POINTING_DEVICE_ENABLE
+#ifdef SPLIT_POINTING_ENABLE
 #    ifndef NO_CHARYBDIS_KEYCODES
 enum charybdis_keycodes {
     POINTER_DEFAULT_DPI_FORWARD = QK_KB_0,
@@ -108,4 +108,4 @@ bool charybdis_get_pointer_dragscroll_enabled(void);
  * are translated into horizontal and vertical scroll movements.
  */
 void charybdis_set_pointer_dragscroll_enabled(bool enable);
-#endif // POINTING_DEVICE_ENABLE
+#endif // SPLIT_POINTING_ENABLE
