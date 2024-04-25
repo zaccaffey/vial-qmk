d35d79af39735042cc822682f5de365ce35cc722
Erik Trinkle <ectrink@gmail.com>
2024-04-25 12:03:47 -0400

    Revert "WIP (not compiling) trying a different structure"
    This reverts commit 6a66496eb6c1970faaa311892d36c8b372f159a3.

--- keyboards/cyboard/cyboard.h
+++ keyboards/cyboard/cyboard.h
@@ -23,43 +23,28 @@
 #ifdef POINTING_DEVICE_ENABLE
 #    ifndef NO_CHARYBDIS_KEYCODES
 enum charybdis_keycodes {
-    LEFT_POINTER_DEFAULT_DPI_FORWARD = QK_KB_0,
-    LEFT_POINTER_DEFAULT_DPI_REVERSE,
-    LEFT_POINTER_SNIPING_DPI_FORWARD,
-    LEFT_POINTER_SNIPING_DPI_REVERSE,
-    LEFT_SNIPING_MODE,
-    LEFT_SNIPING_MODE_TOGGLE,
-    LEFT_DRAGSCROLL_MODE,
-    LEFT_DRAGSCROLL_MODE_TOGGLE,POINTER_DEFAULT_DPI_FORWARD,
-    RIGHT_POINTER_DEFAULT_DPI_REVERSE,
-    RIGHT_POINTER_SNIPING_DPI_FORWARD,
-    RIGHT_POINTER_SNIPING_DPI_REVERSE,
-    RIGHT_SNIPING_MODE,
-    RIGHT_SNIPING_MODE_TOGGLE,
-    RIGHT_DRAGSCROLL_MODE,
-    RIGHT_DRAGSCROLL_MODE_TOGGLE,
+    POINTER_DEFAULT_DPI_FORWARD = QK_KB_0,
+    POINTER_DEFAULT_DPI_REVERSE,
+    POINTER_SNIPING_DPI_FORWARD,
+    POINTER_SNIPING_DPI_REVERSE,
+    SNIPING_MODE,
+    SNIPING_MODE_TOGGLE,
+    DRAGSCROLL_MODE,
+    DRAGSCROLL_MODE_TOGGLE,
 };
 
-#        define L_DPI_MOD POINTER_DEFAULT_DPI_FORWARD
-#        define L_DPI_RMOD POINTER_DEFAULT_DPI_REVERSE
-#        define L_S_D_MOD POINTER_SNIPING_DPI_FORWARD
-#        define L_S_D_RMOD POINTER_SNIPING_DPI_REVERSE
-#        define L_SNIPING SNIPING_MODE
-#        define L_SNP_TOG SNIPING_MODE_TOGGLE
-#        define L_DRGSCRL DRAGSCROLL_MODE
-#        define L_DRG_TOG DRAGSCROLL_MODE_TOGGLE
-#        define R_DPI_MOD POINTER_DEFAULT_DPI_FORWARD
-#        define R_DPI_RMOD POINTER_DEFAULT_DPI_REVERSE
-#        define R_S_D_MOD POINTER_SNIPING_DPI_FORWARD
-#        define R_S_D_RMOD POINTER_SNIPING_DPI_REVERSE
-#        define R_SNIPING SNIPING_MODE
-#        define R_SNP_TOG SNIPING_MODE_TOGGLE
-#        define R_DRGSCRL DRAGSCROLL_MODE
-#        define R_DRG_TOG DRAGSCROLL_MODE_TOGGLE
+#        define DPI_MOD POINTER_DEFAULT_DPI_FORWARD
+#        define DPI_RMOD POINTER_DEFAULT_DPI_REVERSE
+#        define S_D_MOD POINTER_SNIPING_DPI_FORWARD
+#        define S_D_RMOD POINTER_SNIPING_DPI_REVERSE
+#        define SNIPING SNIPING_MODE
+#        define SNP_TOG SNIPING_MODE_TOGGLE
+#        define DRGSCRL DRAGSCROLL_MODE
+#        define DRG_TOG DRAGSCROLL_MODE_TOGGLE
 #    endif // !NO_CHARYBDIS_KEYCODES
 
 /** \brief Return the current DPI value for the pointer's default mode. */
-uint16_t charybdis_get_pointer_default_dpi(bool is_left);
+uint16_t charybdis_get_pointer_default_dpi(void);
 
 /**
  * \brief Update the pointer's default DPI to the next or previous step.
@@ -69,7 +54,7 @@ uint16_t charybdis_get_pointer_default_dpi(bool is_left);
  *
  * The new value is persisted in EEPROM.
  */
-void charybdis_cycle_pointer_default_dpi(bool forward, bool is_left);
+void charybdis_cycle_pointer_default_dpi(bool forward);
 
 /**
  * \brief Same as `charybdis_cycle_pointer_default_dpi`, but do not write to
@@ -78,10 +63,10 @@ void charybdis_cycle_pointer_default_dpi(bool forward, bool is_left);
  * This means that reseting the board will revert the value to the last
  * persisted one.
  */
-void charybdis_cycle_pointer_default_dpi_noeeprom(bool forward, bool is_left);
+void charybdis_cycle_pointer_default_dpi_noeeprom(bool forward);
 
 /** \brief Return the current DPI value for the pointer's sniper-mode. */
-uint16_t charybdis_get_pointer_sniping_dpi(bool is_left);
+uint16_t charybdis_get_pointer_sniping_dpi(void);
 
 /**
  * \brief Update the pointer's sniper-mode DPI to the next or previous step.
@@ -91,7 +76,7 @@ uint16_t charybdis_get_pointer_sniping_dpi(bool is_left);
  *
  * The new value is persisted in EEPROM.
  */
-void charybdis_cycle_pointer_sniping_dpi(bool forward, bool is_left);
+void charybdis_cycle_pointer_sniping_dpi(bool forward);
 
 /**
  * \brief Same as `charybdis_cycle_pointer_sniping_dpi`, but do not write to
@@ -100,10 +85,10 @@ void charybdis_cycle_pointer_sniping_dpi(bool forward, bool is_left);
  * This means that reseting the board will revert the value to the last
  * persisted one.
  */
-void charybdis_cycle_pointer_sniping_dpi_noeeprom(bool forward, bool is_left);
+void charybdis_cycle_pointer_sniping_dpi_noeeprom(bool forward);
 
 /** \brief Whether sniper-mode is enabled. */
-bool charybdis_get_pointer_sniping_enabled(bool is_left);
+bool charybdis_get_pointer_sniping_enabled(void);
 
 /**
  * \brief Enable/disable sniper mode.
@@ -111,10 +96,10 @@ bool charybdis_get_pointer_sniping_enabled(bool is_left);
  * When sniper mode is enabled the dpi is reduced to slow down the pointer for
  * more accurate movements.
  */
-void charybdis_set_pointer_sniping_enabled(bool enable, bool is_left);
+void charybdis_set_pointer_sniping_enabled(bool enable);
 
 /** \brief Whether drag-scroll is enabled. */
-bool charybdis_get_pointer_dragscroll_enabled(bool is_left);
+bool charybdis_get_pointer_dragscroll_enabled(void);
 
 /**
  * \brief Enable/disable drag-scroll mode.
@@ -122,5 +107,5 @@ bool charybdis_get_pointer_dragscroll_enabled(bool is_left);
  * When drag-scroll mode is enabled, horizontal and vertical pointer movements
  * are translated into horizontal and vertical scroll movements.
  */
-void charybdis_set_pointer_dragscroll_enabled(bool enable, bool is_left);
+void charybdis_set_pointer_dragscroll_enabled(bool enable);
 #endif // POINTING_DEVICE_ENABLE
