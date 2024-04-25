87f6e0ef66fb842f5460f233a20315488b18ae83
Erik Trinkle <ectrink@gmail.com>
2024-04-25 11:44:31 -0400

    remove blackpill stuff

--- keyboards/cyboard/cyboard.c
+++ keyboards/cyboard/cyboard.c
@@ -368,17 +368,3 @@ void housekeeping_task_kb(void) {
 }
 #    endif // CHARYBDIS_CONFIG_SYNC
 #endif     // POINTING_DEVICE_ENABLE
-
-#if defined(KEYBOARD_bastardkb_charybdis_3x5_blackpill) || defined(KEYBOARD_bastardkb_charybdis_4x6_blackpill)
-void keyboard_pre_init_kb(void) {
-    setPinInputHigh(A0);
-    keyboard_pre_init_user();
-}
-
-void matrix_scan_kb(void) {
-    if (!readPin(A0)) {
-        reset_keyboard();
-    }
-    matrix_scan_user();
-}
-#endif // KEYBOARD_bastardkb_charybdis_3x5_blackpill || KEYBOARD_bastardkb_charybdis_4x6_blackpill
